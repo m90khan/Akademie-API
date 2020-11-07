9c5f7f85f6288a83495fb8a28eae7a3b6563ce5b
Khan Mohsin <m90khan@gmail.com>
2020-11-07 23:58:25 +0500

    model controllerupdates

--- server.js
+++ server.js
@@ -3,8 +3,25 @@ dotenv.config();
 
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 const app = require('./app');
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log(`DB connected: listening port: ${port}`.bgYellow.bold.black);
+  });
 
+const server = app.listen(port);
 
-const server = app.listen(port)
+// unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //close server and exit process
+  server.close(() => process.exit());
+});
